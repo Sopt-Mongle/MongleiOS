6b59dd5f35a64bf5c31090a3b1c5947cf3e8ccc4
Juhyeoklee <gur6531@naver.com>
2020-07-07 17:29:17 +0900

    [UPDATE] 메인 세번째 셀 완료 & 네번째 셀 구현중

---
+++ Mongle/Mongle/Source/Cell/MainPopularCuratorCVC.swift
@@ -0,0 +1,13 @@
+//
+//  MainPopularCuratorCVC.swift
+//  Mongle
+//
+//  Created by 이주혁 on 2020/07/07.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import UIKit
+
+class MainPopularCuratorCVC: UICollectionViewCell {
+    
+}
---
+++ Mongle/Mongle/Source/Cell/MainTabFourthTVC.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabFourthTVC.swift
+//  Mongle
+//
+//  Created by 이주혁 on 2020/07/07.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import UIKit
+
+class MainTabFourthTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
