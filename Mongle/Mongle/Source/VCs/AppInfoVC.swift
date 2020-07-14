3f29e93c600712bf08c682aecc8da781d0841354
Juhyeoklee <gur6531@naver.com>
2020-07-15 02:06:44 +0900

    [UPDATE] App Info View

---
+++ Mongle/Mongle/Source/VCs/AppInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  AppInfoVC.swift
+//  Mongle
+//
+//  Created by 이주혁 on 2020/07/15.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import UIKit
+
+class AppInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
