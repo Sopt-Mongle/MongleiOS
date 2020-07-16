17a8717750b502c36e5e5f6f30857efb5d668582
seu11ee <yslee2504@naver.com>
2020-07-16 17:34:13 +0900

    [UPDATE] headerview까지 완료 광광 우럭..

--- Mongle/Mongle/Source/VCs/MyTabSentenceVC.swift
+++ Mongle/Mongle/Source/VCs/MyTabSentenceVC.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class MyTabSentenceVC: UIViewController {
+    var sentenceText: String = """
+    처음 마주할 때의 인상, 사소한 것으로 인해 생기는 호
+    감, 알아가면서 느끼는 다양한 감정과 머금고있는 풍
+    경과 분위기까지. 처음 마주할 때의 인상, 사소한 것으
+    로 인해 생기는 호감, 알아가면서 느끼는 다양한 감정
+    과 머금고있는 풍경과 분위기까지. 처음 마주할 때의
+    인상, 사소한 것으로 인해 생기는 호감,
+    """
     var doNotReload = false
     var bookmarkBtnIdx = 0
     @IBOutlet weak var sentenceTableView: UITableView!
@@ -72,21 +80,6 @@ extension MyTabSentenceVC: UITableViewDelegate{
             headerView.addSubview($0)
         }
         
-        if self.doNotReload{
-            savedThemeLabel.textColor = .veryLightPink
-            savedThemeLabel.backgroundColor = .white
-            madeThemeLabel.textColor = .white
-            madeThemeLabel.backgroundColor = .softGreen
-            
-        }
-        else {
-            madeThemeLabel.textColor = .veryLightPink
-            madeThemeLabel.backgroundColor = .white
-            savedThemeLabel.textColor = .white
-            savedThemeLabel.backgroundColor = .softGreen
-        }
-        
-        
         let savedThemeTouchArea = UIButton().then{
             $0.setTitle("", for: .normal)
             $0.backgroundColor = .clear
@@ -103,6 +96,23 @@ extension MyTabSentenceVC: UITableViewDelegate{
             $0.backgroundColor = .veryLightPinkSix
             
             headerView.addSubview($0)
+        }
+//
+        
+        if self.doNotReload{
+            savedThemeLabel.textColor = .veryLightPink
+            savedThemeLabel.backgroundColor = .white
+            madeThemeLabel.textColor = .white
+            madeThemeLabel.backgroundColor = .softGreen
+            
+            
+        }
+        else {
+            madeThemeLabel.textColor = .veryLightPink
+            madeThemeLabel.backgroundColor = .white
+            savedThemeLabel.textColor = .white
+            savedThemeLabel.backgroundColor = .softGreen
+            
         }
         
         savedThemeLabel.snp.makeConstraints{
@@ -162,11 +172,48 @@ extension MyTabSentenceVC: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchResultSentenceTVC", for: indexPath) as? SearchResultSentenceTVC else{
-            return UITableViewCell()
+        if doNotReload{
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "SentenceMoreTVC", for: indexPath) as? SentenceMoreTVC else{
+                return UITableViewCell()
+            }
+            cell.moreBTNDelegate = { [weak self] sheet in
+                let editAction = UIAlertAction(title: "수정", style: .default) { action in
+                    guard let dvc = UIStoryboard.init(name: "SentenceEdit", bundle: nil).instantiateViewController(identifier: "SentenceEditVC") as? SentenceEditVC else {
+                        return
+                    }
+                    dvc.text = self?.sentenceText
+                    self?.navigationController?.pushViewController(dvc, animated: true)
+                }.then {
+                    $0.titleTextColor = .black
+                }
+                
+                let deleteAction = UIAlertAction(title: "삭제", style: .default) { action in
+                    
+                }.then {
+                    $0.titleTextColor = .black
+                }
+                
+                let cancelAction = UIAlertAction(title: "취소", style: .cancel) { action in
+                }.then {
+                    $0.titleTextColor = .black
+                }
+
+                sheet.addAction(editAction)
+                sheet.addAction(deleteAction)
+                sheet.addAction(cancelAction)
+                self?.present(sheet, animated: true, completion: nil)
+            }
+            return cell
+        }
+        else{
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchResultSentenceTVC", for: indexPath) as? SearchResultSentenceTVC else{
+                return UITableViewCell()
+            }
+            return cell
+            
         }
         
-        return cell
+        
     }
     
     
