55b9ce54969ea3ff228b21f22cb27b75f4884596
seu11ee <yslee2504@naver.com>
2020-07-17 02:04:14 +0900

    searchTheme, searchSentence

---
+++ Mongle/Mongle/Source/Model/SearchCuratorData.swift
@@ -0,0 +1,9 @@
+//
+//  SearchCuratorData.swift
+//  Mongle
+//
+//  Created by 이예슬 on 7/17/20.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import Foundation
---
+++ Mongle/Mongle/Source/Model/SearchSentenceData.swift
@@ -0,0 +1,9 @@
+//
+//  SearchSentenceData.swift
+//  Mongle
+//
+//  Created by 이예슬 on 7/17/20.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import Foundation
