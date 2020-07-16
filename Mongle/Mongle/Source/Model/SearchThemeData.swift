28d4ac7ad6c263499e85cd4b9c87472f18612a2a
seu11ee <yslee2504@naver.com>
2020-07-17 00:09:32 +0900

    searchThemeData 컨플릭트 수정

--- Mongle/Mongle/Source/Model/SearchThemeData.swift
+++ Mongle/Mongle/Source/Model/SearchThemeData.swift
@@ -2,25 +2,12 @@
 //  SearchThemeData.swift
 //  Mongle
 //
-<<<<<<< HEAD
-//  Created by 이예슬 on 7/16/20.
-=======
 //  Created by Yunjae Kim on 2020/07/16.
->>>>>>> develop
 //  Copyright © 2020 이주혁. All rights reserved.
 //
 
 import Foundation
 
-<<<<<<< HEAD
-struct searchThemeData: Codable {
-    var themeIdx: Int
-    var theme: String
-    var themeImg: String
-    var themeImgIdx, saves, sentenceNum: Int
-}
-
-=======
 struct SearchThemeData : Codable {
     
     let themeIdx: Int
@@ -30,4 +17,3 @@ struct SearchThemeData : Codable {
     
     
 }
->>>>>>> develop
