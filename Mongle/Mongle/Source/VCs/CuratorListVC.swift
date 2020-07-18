5eef3d2b04ac01eaa527bf58aaa6666e9043269b
seu11ee <yslee2504@naver.com>
2020-07-18 14:57:40 +0900

    큐레이터탭, 내서재 완료:

--- Mongle/Mongle/Source/VCs/CuratorListVC.swift
+++ Mongle/Mongle/Source/VCs/CuratorListVC.swift
@@ -21,15 +21,6 @@ class CuratorListVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 // MARK:- Extensions
