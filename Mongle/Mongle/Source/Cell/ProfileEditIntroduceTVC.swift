31de22e3e10732418510c756841522d28c3442e5
seu11ee <yslee2504@naver.com>
2020-11-13 18:51:17 +0900

    프로필 수정뷰 완료

--- Mongle/Mongle/Source/Cell/ProfileEditIntroduceTVC.swift
+++ Mongle/Mongle/Source/Cell/ProfileEditIntroduceTVC.swift
@@ -16,10 +16,12 @@ class ProfileEditIntroduceTVC: UITableViewCell {
             introduceTextField.autocorrectionType = .no
         }
     }
-    lazy var warningStackView: UIStackView = UIStackView()
+    @IBOutlet weak var introduceWarningImageview: UIImageView!
+    @IBOutlet weak var introduceWarningLabel: UILabel!
     var selectedTextFieldDelegate: (() -> ()) = { }
     var unSelectedTextfieldDelegate: (() -> ()) = { }
     var introduceDelegate: ((String) -> ()) = { _ in }
+    static var isIntroduceValid:Bool?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -29,8 +31,9 @@ class ProfileEditIntroduceTVC: UITableViewCell {
         introduceTextField.setBorder(borderColor: .veryLightPinkFive, borderWidth: 1)
         introduceTextField.addTarget(self, action: #selector(textFieldChanged(sender:)), for: .editingChanged)
         // Initialization code
-        
-        warningStackView = makeWarningStackView(message: "소개를 입력해주세요!", isCorrect: false)
+        introduceTextField.text =
+            UserDefaults.standard.string(forKey: "UserProfileIntroduce")
+        ProfileEditIntroduceTVC.isIntroduceValid = isValidIntroductionInput()
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
@@ -39,62 +42,33 @@ class ProfileEditIntroduceTVC: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
     }
     @objc func textFieldChanged(sender: UITextField){
-        if isValidIntroductionInput() {
-            warningStackView.removeFromSuperview()
-            sender.setBorder(borderColor: .softGreen, borderWidth: 1.0)
+        ProfileEditIntroduceTVC.isIntroduceValid = isValidIntroductionInput()
+        if ProfileEditIntroduceTVC.isIntroduceValid! {
+            hideWarning()
+            
         }
         else {
-            sender.setBorder(borderColor: .reddish, borderWidth: 1.0)
-            self.addSubview(warningStackView)
+            showWarning()
+            
         }
     }
     
-    func makeWarningStackView(message: String, isCorrect: Bool) -> UIStackView{
-
-        var imageStr: String = ""
-        var stateColor: UIColor?
-        
-        if isCorrect {
-            imageStr = "mySettingsProfileNamePossibleIcPossible"
-            stateColor = .softGreen
-        }
-        else {
-            imageStr = "warning"
-            stateColor = .reddish
-        }
-        
-        let label = UILabel().then {
-            $0.text = message
-            $0.textColor = stateColor
-            $0.font = UIFont.systemFont(ofSize: 13)
-            $0.sizeToFit()
-        }
-        
-        let imageView = UIImageView().then {
-            $0.frame = CGRect(x: 0, y: 0, width: 15, height: 15)
-            $0.image = UIImage(named: imageStr)
-        }
-        
-        imageView.snp.makeConstraints {
-            $0.width.height.equalTo(15)
-        }
-        
-        
-        let stackView = UIStackView().then {
-            $0.spacing = 8
-            $0.distribution = .fill
-            $0.axis = .horizontal
-            $0.addArrangedSubview(imageView)
-            $0.addArrangedSubview(label)
-        }
-        
-        return stackView
+    
+    func showWarning(){
+        introduceWarningLabel.alpha = 1
+        introduceWarningImageview.alpha = 1
+        introduceTextField.setBorder(borderColor: .reddish, borderWidth: 1.0)
+    }
+    func hideWarning(){
+        introduceWarningLabel.alpha = 0
+        introduceWarningImageview.alpha = 0
+        introduceTextField.setBorder(borderColor: .veryLightPink, borderWidth: 1.0)
     }
     func isValidIntroductionInput() -> Bool {
         guard let text = introduceTextField.text else {
             return false
         }
-        if text.count > 0 && text.count < 30 {
+        if text.count > 0 && text.count <= 30 {
            return true
         }
         return false
@@ -108,7 +82,6 @@ extension ProfileEditIntroduceTVC: UITextFieldDelegate {
         selectedTextFieldDelegate()
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.setBorder(borderColor: .veryLightPink, borderWidth: 1.0)
         unSelectedTextfieldDelegate()
         introduceDelegate(textField.text!)
         
